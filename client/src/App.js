082ff3188b4f81a1067bb6ea33f92a2af45bdf5f
Alex Hek <alexanderhek@gmail.com>
2021-06-29 12:09:32 +0100

    changed favicon, added credit to favicon author as a div

--- client/src/App.js
+++ client/src/App.js
@@ -44,6 +44,16 @@ const App = () => {
           render={() => (!isSignedIn ? <Register /> : <Redirect to='/' />)}
         />
       </Switch>
+      <div className='favicon-credit'>
+        Favicon made by{' '}
+        <a href='https://www.freepik.com' title='Freepik'>
+          Freepik
+        </a>{' '}
+        from{' '}
+        <a href='https://www.flaticon.com/' title='Flaticon'>
+          www.flaticon.com
+        </a>
+      </div>
     </div>
   );
 };
